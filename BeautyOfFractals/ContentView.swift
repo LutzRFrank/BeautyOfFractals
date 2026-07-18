36ea489ab844e344ee67a1db6671511a1ea6e3ac
Lutz-R. Frank <lutzr.frank@icloud.com>
2026-07-18 11:32:39 +0200

    Raise automatic guarded reference limit

--- BeautyOfFractals/ContentView.swift
+++ BeautyOfFractals/ContentView.swift
@@ -4925,7 +4925,7 @@ nonisolated private func renderDirectMandelbrotTripleDoubleParallel(
     // perturbation arithmetic.
     let usesGuardedReference = abs(projectedScale) < 1e-42
     let referenceIterationBudget = usesGuardedReference && automaticIterationsEnabled
-        ? 250_000
+        ? 500_000
         : maxIterations
     let centralGuardedReference = usesGuardedReference
         ? TripleDoublePerturbation.makeGuardedCentralReference(
